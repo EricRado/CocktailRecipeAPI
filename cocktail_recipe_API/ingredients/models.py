255f454825453f83c9e72409c77f372bd373c3df
Eric Rado <erado@axxess.com>
2020-05-26 21:08:03 -0400

    Updated Ingredient id field to primary key

--- cocktail_recipe_API/ingredients/models.py
+++ cocktail_recipe_API/ingredients/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Ingredient(models.Model):
-    id = models.CharField(max_length=15)
+    id = models.CharField(max_length=15, primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
     type = models.CharField(max_length=30)
