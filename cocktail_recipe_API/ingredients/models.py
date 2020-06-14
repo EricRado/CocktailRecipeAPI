19da545124317f69a6fe3f032fc24e5aba4b9258
Eric Rado <erado@axxess.com>
2020-06-14 00:12:43 -0400

    Meta data added to Ingredient model and UUIDField. Migrated change

--- cocktail_recipe_API/ingredients/models.py
+++ cocktail_recipe_API/ingredients/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+import uuid
 
 class Ingredient(models.Model):
-    id = models.CharField(max_length=15, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
     type = models.CharField(max_length=30)
     thumbURL = models.URLField()
+
+    class Meta:
+        db_table = "ingredient"
+        verbose_name = "Ingredient"
+        verbose_name_plural = "Ingredients"
